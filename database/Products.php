00d312112b5d2a9369989ca97233a595158a7c58
Juanchis <juanchismo10@gmail.com>
2021-12-12 21:35:27 -0300

    Agrego el select de categorías de producto y su función que lo trae desde la base de datos

--- database/Products.php
+++ database/Products.php
@@ -4,31 +4,51 @@ require_once 'connection.php';
 class Products extends Connection
 {
     private $message;
-    function validation_fields_products($name, $price, $photo, $comment)
+    private $categories;
+    private $table;
+
+    // fields validation
+    function validation_fields_products($name, $price, $photo, $categories)
     {
         $this->message = '';
+
         if (empty($name)) {
-            $this->message = 'Ingrese el nombre';
+            $this->message .= 'Ingrese el nombre.<br>';
         } else {
             if (strlen($name) < 3) {
-                $this->message = 'El nombre debe tener 3 caractéres. ';
+                $this->message .= 'El nombre debe tener 3 caractéres. <br>';
             }
         }
 
         if (empty($price)) {
-            $this->message = 'Ingrese el precio.';
+            $this->message .= 'Ingrese el precio.<br>';
         } else {
             if (!is_numeric($price)) {
-                $this->message = 'El precio debe ser un número';
+                $this->message .= 'El precio debe ser numérico.<br>';
             }
         }
 
         if (!empty($photo)) {
             if ($photo != 'image/jpg' || $photo != 'image/png') {
-                $this->message = 'La imagen debe ser png o jpg';
+                $this->message .= 'La imagen debe ser png o jpg.<br>';
             }
         }
 
+        if (empty($categories)) {
+            $this->message .= 'Debe colocar una categoría.<br>';
+        }
+
         return $this->message;
+    }
+
+    function show_categories()
+    {
+        $this->categories = [];
+        $sql = "SELECT `cod_cat`, `desc_cat` FROM categories";
+        $statement = $this->connect()->prepare($sql);
+        $statement->execute();
+        $this->categories = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        return $this->categories;
     }
 }
--- index.php
+++ index.php
@@ -6,6 +6,10 @@ if (empty($_SESSION['name'])) {
 }
 require_once 'database/Products.php';
 
+// mostrar categorías   
+$sel = new Products;
+$select = $sel->show_categories();
+
 $message = '';
 if (isset($_POST['btn-save'])) {
     $product = new Products;
@@ -31,17 +35,30 @@ if (isset($_POST['btn-save'])) {
             <div class="col-sm-6">
                 <form class="needs-validation" method="POST" enctype="multipart/form-data">
                     <div class="form-row">
-                        <div class="col-md-4 mb-3">
+                        <div class="col-md-6 mb-3">
                             <label for="validationCustom01">Producto:</label>
-                            <input type="text" class="form-control" name="product" placeholder="Producto" value="">
-
-                        </div>
-                        <div class="col-md-4 mb-3">
-                            <label for="validationCustom02">Precio:</label>
-                            <input type="text" class="form-control" name="price" placeholder="Precio" value="">
+                            <input type="text" class="form-control" name="product" placeholder="Producto" value="<?php echo (!empty($_POST['product'])) ? $_POST['product'] : '' ?>">
 
                         </div>
                         <div class="col-md-6 mb-3">
+                            <label for="validationCustom02">Precio:</label>
+                            <input type="text" class="form-control" name="price" placeholder="Precio" value="<?php echo (!empty($_POST['price'])) ? $_POST['price'] : '' ?>">
+
+                        </div>
+                        <div class="col-md-12 mb-3">
+                            <label for="validationCustom02">Precio:</label>
+                            <select class="form-select col-md-12" aria-label="Default select example">
+                                <option selected>Elige una categoría</option>
+                                <?php
+                                foreach ($select as $row) { ?>
+                                    <option value="<?php echo $row['cod_cat'] ?>"><?php echo $row['desc_cat']; ?></option>
+                                <?php
+                                }
+                                ?>
+                            </select>
+                        </div>
+
+                        <div class="col-md-12 mb-3">
                             <div class="mb-3">
                                 <label for="formFileSm" class="form-label">Foto del producto:</label>
                                 <input class="form-control form-control-sm" type="file" name="img-file">
@@ -50,13 +67,13 @@ if (isset($_POST['btn-save'])) {
                         </div>
                     </div>
                     <div class="form-row">
-                        <div class="mb-3 col-md-8">
+                        <div class="mb-3 col-md-12">
                             <label for="exampleFormControlTextarea1" class="form-label">¿Algún comentario?</label>
                             <textarea class="form-control" name="comment" rows="3"></textarea>
                             <br>
                         </div>
                         <?php if (!empty($message)) { ?>
-                            <div class="alert alert-danger col-md-8" role="alert"><?php echo $message; ?></div>
+                            <div class="alert alert-danger col-md-12" role="alert"><?php echo $message; ?></div>
 
                         <?php } ?>
                     </div>
